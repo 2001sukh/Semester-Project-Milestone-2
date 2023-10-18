0f9d67fb5606650a76e2f0b63c373a14430679fe
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 17:21:45 -0700

    Add a <header> element

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" a href="style.css">
     <title>Semester Project Milestone-2</title>
 </head>
-<body>
 
+<body>
+    <header>
+        
+    </header>
 
 </body>
+
 </html>
