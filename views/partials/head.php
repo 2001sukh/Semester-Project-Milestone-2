9359db0420bb99501ae0cce55a86ea1c564c4dfc
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 19:56:15 -0700

    Add head element of all pages in new page called head.php

---
+++ views/partials/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" a href="style.css">
+    <title>Semester Project Milestone-2</title>
+</head>
