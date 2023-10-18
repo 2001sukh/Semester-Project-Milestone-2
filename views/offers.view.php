b64d9b17a6cc397e30b7df1a977209a900de67f2
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 23:03:12 -0700

    Add HTML code for Food Item 1 in the menu

--- views/offers.view.php
+++ views/offers.view.php
@@ -3,6 +3,25 @@
 
 <body>
 <h1>Now you are on a offers page </h1>
+
+<h2>Restaurant Menu</h2>
+
+<div class="menu-items">
+    <div class="menu-item">
+        <img src="images/food-item-1.jpg" alt="Food Item 1">
+        <h3>Food Item 1</h3>
+        <p>Description of Food Item 1.</p>
+        <p><strong>Price: $10.99</strong></p>
+    </div>
+
+
+
+    
+  
+  
+  
+</div>
+
 </body>
 
 <?php require('partials/footer.php') ?>
