01d6f174036d69d19f2e5511b18b285164ffdb69
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 23:33:09 -0700

    Add text in <p> tag

--- views/about.view.php
+++ views/about.view.php
@@ -5,8 +5,20 @@
     <h1>Now you are on a About page</h1>
     <div class="aboutimage">
     <img src="images/side-view-potatoes-with-meat-tomato-sauce-with-arugula-greek-salad-soup-table.jpg">
+    <p>Hungry Hub is more than just a typical food delivery app; it's a comprehensive platform designed to enhance and simplify the entire delivery experience for users. Our mission is to provide a one-stop solution for all your delivery needs, whether it's a delicious meal from your favorite restaurant, essential groceries, important packages, or any other type of delivery requirement.
+
+In today's fast-paced world, convenience is paramount, and that's where Hungry Hub comes in. We understand that your time is valuable, and our goal is to save you time and effort by connecting you with a vast network of reliable delivery partners. With Hungry Hub, you can trust that your items will be delivered to your doorstep promptly and conveniently.
+
+Our commitment to excellence extends to every aspect of the delivery process. When you use Hungry Hub, you can expect efficient and secure handling of your deliveries. Whether you're craving a restaurant-quality meal or need your groceries urgently, our platform ensures that you have a seamless and stress-free experience.
+
+Hungry Hub takes pride in being your number one choice for delivery services. We're dedicated to providing a wide selection of delicious dishes, groceries, and essential items, all available at your fingertips. By using Hungry Hub, you'll experience the ultimate convenience in fast, reliable, and hassle-free delivery services.
+
+Our platform is not limited to just one type of delivery; it's your go-to solution for any item you need to be delivered. So, whether you're in the mood for a delectable meal, require groceries for the week, or need something delivered urgently, you can count on Hungry Hub to connect you with trusted delivery partners who will ensure your items are safely and promptly delivered.
+
+Hungry Hub is all about simplifying your life, providing convenience, and ensuring that your cravings and needs are satisfied with just a few clicks. So why wait? Order now and experience the ease and efficiency of the Hungry Hub delivery experience. Welcome to a world where fast, reliable, and convenient delivery is just a tap away.</P>
     </div>
 
+    
 
 </body>
 
