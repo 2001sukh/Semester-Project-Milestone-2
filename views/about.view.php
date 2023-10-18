a140bf702fc97eeb539470ccbc8808b2a301b8f3
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 19:40:27 -0700

    Add Nav in new file called 'nav.php' in partials folder

--- views/about.view.php
+++ views/about.view.php
@@ -9,19 +9,7 @@
 </head>
 
 <body>
-    <header>
-        <nav>
-            <div class="logo">
-                <img src="logo.png" alt="Logo">
-            </div>
-            <ul class="menu">
-                <li><a href="/">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="offers.php">Offers</a></li>
-                <li><a href="restaurants.php">Restaurants</a></li>
-            </ul>
-        </nav>
-</header>
+   
 </body>
 
 <h1>Now you are on a About  page</h1>
