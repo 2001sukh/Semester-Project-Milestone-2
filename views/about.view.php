d6802358913799fcaecec2cfeb847e425aa5afeb
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 19:52:12 -0700

    format documents in about.view.php

--- views/about.view.php
+++ views/about.view.php
@@ -7,12 +7,15 @@
     <link rel="stylesheet" a href="style.css">
     <title>Semester Project Milestone-2</title>
 </head>
-<?php require('partials/nav.php')?>
+
+<?php require('partials/nav.php') ?>
+
 <body>
-<h1>Now you are on a About  page</h1>
+    <h1>Now you are on a About page</h1>
 </body>
+
 <footer>
-        <p>&copy; 2023 Your Website. All rights reserved.</p>
-    </footer>
+    <p>&copy; 2023 Your Website. All rights reserved.</p>
+</footer>
 
 </html>
