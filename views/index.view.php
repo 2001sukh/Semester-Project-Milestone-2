b510bcb37dfff01bf9e29ba59dcdd26010bb6649
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 22:30:34 -0700

    Add HTML code for a restaurant section 1

--- views/index.view.php
+++ views/index.view.php
@@ -4,6 +4,13 @@
 <body>
     <div class="restaurants">
 
+    <div class="restaurant">
+            <img src="images/restaurant-interior.jpg" alt="Restaurant 1">
+            <h2>Restaurant 1</h2>
+            <p>Description of Restaurant 1.</p>
+            <a href="offers.php" class="menu-button">View Menu</a>
+        </div>
+
     </div>
 
 
