dc3e0e9bfe68639d6f781cba8b85303356bb3854
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 19:01:22 -0700

    Create restaurants.view.php

---
+++ restaurants.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" a href="style.css">
+    <title>Semester Project Milestone-2</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="logo.png" alt="Logo">
+            </div>
+            <ul class="menu">
+                <li><a href="/">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="offers.php">Offers</a></li>
+                <li><a href="#">Restaurants</a></li>
+                <li><a href="#">Profile</a></li>
+            </ul>
+        </nav>
+</header>
+</body>
+
+<h1>Now you are on a home page</h1>
+
+<footer>
+        <p>&copy; 2023 Your Website. All rights reserved.</p>
+    </footer>
+
+</html>
