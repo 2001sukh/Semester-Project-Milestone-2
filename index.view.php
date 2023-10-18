d64897f3232f8d47433d5e8428474fac9f305054
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-17 18:37:35 -0700

    Update content in elements

--- index.view.php
+++ index.view.php
@@ -1,3 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require "index.view.php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" a href="style.css">
+    <title>Semester Project Milestone-2</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="logo.png" alt="Logo">
+            </div>
+            <ul class="menu">
+                <li><a href="/">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Offers</a></li>
+                <li><a href="#">Restaurants</a></li>
+                <li><a href="#">Profile</a></li>
+            </ul>
+        </nav>
+</header>
+</body>
+
+<h1>Now you are on a home page</h1>
+
+<footer>
+        <p>&copy; 2023 Your Website. All rights reserved.</p>
+    </footer>
+
+</html>
