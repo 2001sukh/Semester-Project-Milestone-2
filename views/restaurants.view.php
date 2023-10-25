505d8e8b06b148e2f1d454fce0f72404a6d2b6de
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-25 12:19:22 -0700

    Add empty cta-section <div> element

--- views/restaurants.view.php
+++ views/restaurants.view.php
@@ -2,7 +2,11 @@
 <?php require('partials/nav.php') ?>
 
 <body>
-<h1>Now you are on a Restaurants page</h1>
+    <h1>Now you are on the Restaurant page</h1>
+    
+    <div class="cta-section">
+        
+    </div>
 </body>
 
 <?php require('partials/footer.php') ?>
