6f8ef68d2063e722c4c24027795a7c13a805e15c
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-31 12:50:48 -0700

    Add CSS for the carousel image  3 in carousel-slide

--- views/index.view.php
+++ views/index.view.php
@@ -9,6 +9,10 @@
   <div class="carousel-slide">
     <img src="images\side-view-potatoes-with-meat-tomato-sauce-with-arugula-greek-salad-soup-table.jpg" alt="Carousel Slide 2">
   </div>
+  <div class="carousel-slide">
+    <img src="images\wine-glass.jpg" alt="Carousel Slide 3">
+  </div>
+
 
 
 <?php require('partials/footer.php') ?>
