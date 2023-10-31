4c0df72e1b266128e989b11ae0c26d279a3f1730
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-31 12:53:59 -0700

    Add  slideIndex and call showSlides in a <div class="carousel">

--- views/index.view.php
+++ views/index.view.php
@@ -16,6 +16,13 @@
   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
   <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
-</body>
 
+<!-- JavaScript for the carousel -->
+<script>
+  let slideIndex = 0;
+  showSlides();
+</script>
+
+
+</body>
 <?php require('partials/footer.php') ?>
