a5a5605e40158802b9702e1459e77c5be77327cb
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-10-31 16:23:00 -0700

    Add appropriate  images in grid containers

--- views/index.view.php
+++ views/index.view.php
@@ -55,37 +55,37 @@
 <div class="grid-container">
   <div class="grid-item">
     <a href="link1.html">
-      <img src="images/grid1.jpg" alt="Grid Item 1">
+      <img src="images\bartender-making-alcoholic-cocktail-summer-cocktail-bar.jpg" alt="Grid Item 1">
       <button class="order-button">Order Now</button>
     </a>
   </div>
   <div class="grid-item">
     <a href="link2.html">
-      <img src="images/grid2.jpg" alt="Grid Item 2">
+      <img src="images\man-hand-pours-larger-beer-glass-standing-vintage-table-blurred-counter-bar-jar-pickles.jpg" alt="Grid Item 2">
       <button class="order-button">Order Now</button>
     </a>
   </div>
   <div class="grid-item">
     <a href="link3.html">
-      <img src="images/grid3.jpg" alt="Grid Item 3">
+      <img src="images\side-view-potatoes-with-meat-tomato-sauce-with-arugula-greek-salad-soup-table.jpg" alt="Grid Item 3">
       <button class="order-button">Order Now</button>
     </a>
   </div>
   <div class="grid-item">
     <a href="link4.html">
-      <img src="images/grid4.jpg" alt="Grid Item 4">
+      <img src="images\mini-burgers-french-fries-spices-cocktails-side-view.jpg" alt="Grid Item 4">
       <button class="order-button">Order Now</button>
     </a>
   </div>
   <div class="grid-item">
     <a href="link5.html">
-      <img src="images/grid5.jpg" alt="Grid Item 5">
+      <img src="images\side-view-woman-put-cup-tea-lemon-slice-pancakes-with-boiled-eggs-tomatoes-cucumbers-honey-table-serve-breakfast.jpg" alt="Grid Item 5">
       <button class="order-button">Order Now</button>
     </a>
   </div>
   <div class="grid-item">
     <a href="link6.html">
-      <img src="images/grid6.jpg" alt="Grid Item 6">
+      <img src="images\various-snacks-served-with-beer.jpg" alt="Grid Item 6">
       <button class="order-button">Order Now</button>
     </a>
   </div>
