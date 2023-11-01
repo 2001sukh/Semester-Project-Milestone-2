0d037e8388f61842bf7436ce97f96070548f932f
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 16:28:25 -0700

    Add 5 more dish items in menu page

--- views/menu.view.php
+++ views/menu.view.php
@@ -20,7 +20,34 @@
                 <p>$12.99</p>
             </a>
         </div>
-       
+        <div class="dish-item">
+            <a href="#dish3">
+                <img src="images\side-view-woman-put-cup-tea-lemon-slice-pancakes-with-boiled-eggs-tomatoes-cucumbers-honey-table-serve-breakfast.jpg" alt="Dish 3">
+                <h3>Dish 3</h3>
+                <p>$9.99</p>
+            </a>
+        </div>
+        <div class="dish-item">
+            <a href="#dish4">
+                <img src="images\mini-burgers-french-fries-spices-cocktails-side-view.jpg" alt="Dish 4">
+                <h3>Dish 4</h3>
+                <p>$14.99</p>
+            </a>
+        </div>
+        <div class="dish-item">
+            <a href="#dish5">
+                <img src="images\man-hand-pours-larger-beer-glass-standing-vintage-table-blurred-counter-bar-jar-pickles.jpg" alt="Dish 5">
+                <h3>Dish 5</h3>
+                <p>$8.99</p>
+            </a>
+        </div>
+        <div class="dish-item">
+            <a href="#dish6">
+                <img src="images\bartender-making-alcoholic-cocktail-summer-cocktail-bar.jpg" alt="Dish 6">
+                <h3>Dish 6</h3>
+                <p>$15.99</p>
+            </a>
+        </div>
     </div>
 
 
