95469f44633dc33aec0913b62bb8be20f817b4e6
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 10:30:51 -0700

    Add background container and image element

--- views/restaurants.view.php
+++ views/restaurants.view.php
@@ -2,7 +2,10 @@
 <?php require('partials/nav.php') ?>
 
 <body>
-    
+<div class="background-container">
+        <div class="background-image"></div>
+    </div>
+
 </body>
 
 <?php require('partials/footer.php') ?>
