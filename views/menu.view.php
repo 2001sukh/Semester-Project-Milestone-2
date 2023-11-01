75e931e0a067bb55727d6632a033a0839288498f
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 16:12:08 -0700

    Add 'Feed Me' link in navigation menu

--- views/menu.view.php
+++ views/menu.view.php
@@ -5,66 +5,6 @@
 <h1>Now you are on a offers page </h1>
 <h2>Restaurant Menu</h2>
 
-<div class="menu-items">
-    <div class="menu-item">
-        <img src="#" alt="Food Item 1">
-        <h3>Food Item 1</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 2">
-        <h3>Food Item 2</h3>
-        <p>Description of Food Item 2.</p>
-        <p><strong>Price: $12.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 3">
-        <h3>Food Item 3</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 4">
-        <h3>Food Item 4</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 5">
-        <h3>Food Item 5</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 6">
-        <h3>Food Item 6</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 7">
-        <h3>Food Item 7</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-
-    <div class="menu-item">
-        <img src="#" alt="Food Item 8">
-        <h3>Food Item 8</h3>
-        <p>Description of Food Item 1.</p>
-        <p><strong>Price: $10.99</strong></p>
-    </div>
-    <!-- Add more menu items as needed -->
-
-</div>
-
 </body>
 
 <?php require('partials/footer.php') ?>
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,13 +1,11 @@
 <header>
         <nav>
-        
-       
             <ul class="menu">
                 <li><a href="/">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="menu.php">Menu</a></li>
                 <li><a href="contact.php">Contact</a></li>
-                <li><a href="feedme.php">Feed Me</a></li>
+                <li><a href="menu.php">Feed Me</a></li>
             </ul>
         </nav>
 </header>
