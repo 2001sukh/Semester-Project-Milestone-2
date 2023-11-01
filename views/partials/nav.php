a3cb09ea97c231e6dea09f072fc6e5cc5434cc90
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 10:25:38 -0700

    Add 'Feed Me' link to navigation menu

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,14 +1,13 @@
 <header>
         <nav>
-            <div class="logo">
-                <img src="logo.png" alt="Logo">
-            </div>
+        
        
             <ul class="menu">
                 <li><a href="/">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="offers.php">Offers</a></li>
                 <li><a href="restaurants.php">Restaurants</a></li>
+                <li><a href="feedme.php">Feed Me</a></li>
             </ul>
         </nav>
 </header>
--- views/restaurants.view.php
+++ views/restaurants.view.php
@@ -2,12 +2,7 @@
 <?php require('partials/nav.php') ?>
 
 <body>
-    <h1>Now you are on the Restaurant page</h1>
     
-    <div class="cta-section">
-        <p>Discover our delicious menu and taste the finest dishes in town!</p>
-        <a href="offers.php" class="cta-button">View Menu</a>
-    </div>
 </body>
 
 <?php require('partials/footer.php') ?>
