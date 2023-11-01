689cfce7c5e6d8e0bd9ef2eab3129ecdba71b705
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 16:16:12 -0700

    Add menu item "Dish 1" to menu page

--- views/menu.view.php
+++ views/menu.view.php
@@ -5,6 +5,17 @@
 <h1>Now you are on a offers page </h1>
 <h2>Restaurant Menu</h2>
 
+<div class="dish-container">
+        <div class="dish-item">
+            <a href="contact.php">
+                <img src="images\crispy-french-fries-with-ketchup-mayonnaise.jpg" alt="Dish 1">
+                <h3>Dish 1</h3>
+                <p>$10.99</p>
+            </a>
+        </div>
+        
+
+
 </body>
 
 <?php require('partials/footer.php') ?>
