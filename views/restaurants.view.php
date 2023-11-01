09fbfe31e8001e688e237f654542e07e55b774a5
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 10:47:36 -0700

    Add phone and email input fields

--- views/restaurants.view.php
+++ views/restaurants.view.php
@@ -11,6 +11,12 @@
         <form action="submit-feedback.php" method="post">
             <label for="name">First & Last Name</label>
             <input type="text" id="name" name="name" required>
+
+            <label for="phone">Phone Number</label>
+            <input type="tel" id="phone" name="phone" required>
+
+            <label for="email">Your email address</label>
+            <input type="email" id="email" name="email" required>
         </form>
     </div>
 
