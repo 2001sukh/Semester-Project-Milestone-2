5e52cac56900091acc3544faba31ddfac960afa4
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-01 10:46:30 -0700

    Add a feedback form to collect user information

--- views/restaurants.view.php
+++ views/restaurants.view.php
@@ -6,6 +6,14 @@
         <div class="background-image"></div>
     </div>
 
+    <div class="formtext-container">
+        <h2>Feedback Form</h2>
+        <form action="submit-feedback.php" method="post">
+            <label for="name">First & Last Name</label>
+            <input type="text" id="name" name="name" required>
+        </form>
+    </div>
+
 </body>
 
 <?php require('partials/footer.php') ?>
