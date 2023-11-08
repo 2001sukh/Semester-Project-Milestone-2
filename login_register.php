a42f60bbe1ba90ccb3feadeb7f0e78ed316862ba
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-07 23:14:24 -0800

    Add user existence check during registration

--- login_register.php
+++ login_register.php
@@ -1,3 +1,10 @@
 <?php
 
 require('connection.php');
+
+if(isset($_POST['register']))
+{
+$user_exist_query="SELECT * FROM `registered_users` WHERE `username`='$_POST[username]' OR `email`='$_POST[email]' ";
+$result=mysqli_query($con,$user_exist_query);
+}
+?>
