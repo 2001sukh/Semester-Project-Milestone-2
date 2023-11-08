94d7e84bf3af010817e7992d9c87c119ac4b10b3
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-07 23:16:20 -0800

    Check for existing username in registration process

--- login_register.php
+++ login_register.php
@@ -7,4 +7,23 @@ if(isset($_POST['register']))
 $user_exist_query="SELECT * FROM `registered_users` WHERE `username`='$_POST[username]' OR `email`='$_POST[email]' ";
 $result=mysqli_query($con,$user_exist_query);
 }
+
+if($result)
+
+ if(mysqli_num_rows($result)>0)
+ {
+    $result_fetch=mysqli_fetch_assoc($result);
+    if($result_fetch['username']==$_POST['username'])
+    {
+        echo"
+        <script>
+        alert('$result_fetch[username] - Username already taken');
+        window.location.href='index.php';
+        </script>
+        ";
+    }
+     
+ }
+ 
+
 ?>
