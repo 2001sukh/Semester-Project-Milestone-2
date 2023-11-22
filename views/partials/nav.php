94a36abad4368e0f4d3f89f24b867f27c2d7ef20
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-21 23:42:05 -0800

    update code

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -5,7 +5,6 @@
       <button type='button' onclick="popup('register-popup')">REGISTER</button>
     </div>
             <ul class="menu">
-                <li><a href="/">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="menu.php">Menu</a></li>
                 <li><a href="contact.php">Contact</a></li>
