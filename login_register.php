8d804c72fa4138246e1838f2bd7ba7fd4a65ec17
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-28 16:17:20 -0800

    Retrieve password securely

--- login_register.php
+++ login_register.php
@@ -5,4 +5,5 @@ require('connection.php');
 # For login
 if (isset($_POST['login'])) {
     $email_username = mysqli_real_escape_string($con, $_POST['email_username']);
+    $password = $_POST['password'];
 }
