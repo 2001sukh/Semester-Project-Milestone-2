bf8b04ff9815d1419992175535fdeccb7f00d266
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-28 17:28:21 -0800

    Add login and register buttons for logged-out users

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,9 +1,15 @@
 <header>
     <nav>
-     <div class='sign-in-up'>
+        <div class='sign-in-up'>
             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
                 <!-- Display username and logout link if user is logged in -->
                 <div><?php echo htmlspecialchars($_SESSION['username']) . " - <a href='logout.php'>LOGOUT</a>"; ?></div>
+                <?php else: ?>
+                <!-- Show these buttons if user is not logged in -->
+                <button type='button' onclick="popup('login-popup')">LOGIN</button>
+                <button type='button' onclick="popup('register-popup')">REGISTER</button>
+            <?php endif; ?>
+            
             <ul class="menu">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
