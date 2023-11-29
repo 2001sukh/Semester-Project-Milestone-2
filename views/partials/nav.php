8d8249eb8926b7f0a107c2a6b409e57db7a09e28
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-28 17:24:25 -0800

    Display user info and logout link when logged in

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,9 +1,9 @@
 <header>
     <nav>
-      <div class='sign-in-up'>
-      <button type='button' onclick="popup('login-popup')">LOGIN</button>
-      <button type='button' onclick="popup('register-popup')">REGISTER</button>
-    </div>
+     <div class='sign-in-up'>
+            <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                <!-- Display username and logout link if user is logged in -->
+                <div><?php echo htmlspecialchars($_SESSION['username']) . " - <a href='logout.php'>LOGOUT</a>"; ?></div>
             <ul class="menu">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
