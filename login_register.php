4e598c54012081c93547417b976398626d7ed744
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-28 16:55:34 -0800

    Add registration success message and redirection

--- login_register.php
+++ login_register.php
@@ -55,6 +55,8 @@ if (isset($_POST['register'])) {
                 $hashed_password = password_hash($password, PASSWORD_BCRYPT);
                 $insert_stmt = $con->prepare("INSERT INTO `registered_users`(`full_name`, `username`, `email`, `password`) VALUES (?, ?, ?, ?)");
                 $insert_stmt->bind_param("ssss", $fullname, $username, $email, $hashed_password);
-            }}}
+                if ($insert_stmt->execute()) {
+                    echo "<script>alert('Registration Successful'); window.location.href='index.php';</script>";
+                } }}}
 }
 ?>
