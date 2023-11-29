7a969c7babc7183b746a7d9673402f9273a50c43
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-29 00:50:21 -0800

    Use project root variable for path in session management

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
+// Define the project root directory
+$proj_root = "/dgl123/project/";
 
 session_start();
 session_unset();
 session_destroy();
-header("location:index.php")
-
-
+header("location: " . $proj_root . "index.php");
 ?>
