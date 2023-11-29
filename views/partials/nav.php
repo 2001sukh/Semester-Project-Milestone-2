2f22ca0ee176c36eaa48875a8a429f82820681b4
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-28 17:29:37 -0800

    Format (arrange) code in nav.php for good readability

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -4,18 +4,18 @@
             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
                 <!-- Display username and logout link if user is logged in -->
                 <div><?php echo htmlspecialchars($_SESSION['username']) . " - <a href='logout.php'>LOGOUT</a>"; ?></div>
-                <?php else: ?>
+            <?php else: ?>
                 <!-- Show these buttons if user is not logged in -->
                 <button type='button' onclick="popup('login-popup')">LOGIN</button>
                 <button type='button' onclick="popup('register-popup')">REGISTER</button>
             <?php endif; ?>
-            
-            <ul class="menu">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="menu.php">Menu</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="menu.php">Feed Me</a></li>
-            </ul>
-        </nav>
+        </div>
+        <ul class="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="menu.php">Feed Me</a></li>
+        </ul>
+    </nav>
 </header>
