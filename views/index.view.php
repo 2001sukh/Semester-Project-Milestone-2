57e90d3153a3b952c185773229cdc56778daf1bc
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-29 00:36:27 -0800

    Refactor file paths using $proj_root variable with partials/head.php

--- views/index.view.php
+++ views/index.view.php
@@ -1,8 +1,6 @@
 <?php 
-session_start();
-require('partials/head.php');
-require('partials/nav.php'); 
-require('connection.php');
+session_start(); 
+require($proj_root . "partials/head.php");
 ?>
 
 <body>
