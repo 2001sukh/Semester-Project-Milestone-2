d9789d52b06b5e9977b6d48d215864a51df87d9d
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-11-28 16:19:07 -0800

    Refactor user query with prepared statement

--- login_register.php
+++ login_register.php
@@ -1,9 +1,13 @@
 <?php
 
 require('connection.php');
-
 # For login
 if (isset($_POST['login'])) {
     $email_username = mysqli_real_escape_string($con, $_POST['email_username']);
     $password = $_POST['password'];
+
+ // Prepared statement to prevent SQL Injection
+ $stmt = $con->prepare("SELECT * FROM `registered_users` WHERE `email` = ? OR `username` = ?");
+
+
 }
